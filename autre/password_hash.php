945e123212876c1756a406eee03e2508c1cc48d6
Franck PANEL <franck.panel56@gmail.com>
2024-02-09 01:47:53 +0100

    Suppression de fichiers non nécessaires

--- autre/password_hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Le mot de passe que Vincent Parrot à choisit
-$password = "&éVincent_Parrotè&";
-
-// Générer un hash sécurisé du mot de passe
-$passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-echo $passwordHash; // Je récupere le hash généré
-?>
