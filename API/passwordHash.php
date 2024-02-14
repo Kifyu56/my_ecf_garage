214e458346d9c4cf377791810bb10b496f4bb0e6
Franck PANEL <franck.panel56@gmail.com>
2024-02-14 01:48:25 +0100

    Mise à jour du fichier .gitignore et du mot de passe de hachage

--- API/passwordHash.php
+++ API/passwordHash.php
@@ -1,5 +1,5 @@
 <?php
-$motDePasse = '&éVincent_Parrotè&'; // Remplacez 'votreMotDePasse' par le mot de passe que vous souhaitez hacher
+$motDePasse = '&éKifyu_Testè&'; // Remplacez 'votreMotDePasse' par le mot de passe que vous souhaitez hacher
 $hachage = password_hash($motDePasse, PASSWORD_DEFAULT);
 
 if (password_verify($motDePasse, $hachage)) {
