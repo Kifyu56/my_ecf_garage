d44c415c106f7fb3293d9db282ad8e3b9d34afbd
Kifyu DevLand <franck.panel56@gmail.com>
2024-02-14 21:17:54 +0100

    dashboard_admin

--- admin/dashboard_admin.php
+++ admin/dashboard_admin.php
@@ -21,14 +21,3 @@
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
-
-<label for="exampleDataList" class="form-label">Datalist example</label>
-<input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-<datalist id="datalistOptions">
-    <option value="San Francisco">
-    <option value="New York">
-    <option value="Seattle">
-    <option value="Los Angeles">
-    <option value="Chicago">
-</datalist>
